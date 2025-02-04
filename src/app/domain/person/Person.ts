7d1c7fa55e0e436e89b4490c004db8f1f8ac40fc
SergiiBartytskyi <s.bartycjkyj@gmail.com>
2025-02-04 14:12:05 +0100

    add middlewares

--- src/app/domain/person/Person.ts
+++ src/app/domain/person/Person.ts
@@ -1,29 +1,63 @@
-import { JsonController, Get, Post, Body, Param } from "routing-controllers";
+import {
+  JsonController,
+  Get,
+  Post,
+  Body,
+  Param,
+  UseAfter,
+} from 'routing-controllers';
+import { validate } from 'class-validator';
 
-import type { IPerson } from "./Person.types";
+import { IPerson } from './Person.types';
+import { CreatePerson } from './CreatePerson.dto';
+
+import { ApiResponse } from 'helpers/ApiResponse';
+import { ApiError } from 'helpers/ApiError';
+import { HTTPResponseLogger } from 'app/middlewares/HTTPResponseLogger';
 
 const storeData: IPerson[] = [];
 
-@JsonController("/person")
+@JsonController('/person')
 export default class Person {
   @Get()
+  @UseAfter(HTTPResponseLogger)
   async getAll() {
-    return storeData;
+    return new ApiResponse(true, storeData);
   }
 
-  @Get("/:id")
-  async getOne(@Param("id") id: number): Promise<IPerson | {}> {
+  @Get('/:id')
+  async getOne(@Param('id') id: number): Promise<ApiResponse<IPerson | {}>> {
     const person = storeData.find((item) => {
       return item.id === id;
     });
 
-    return person || {};
+    if (!person) {
+      throw new ApiError(404, {
+        code: 'PERSON_NOT_FOUND',
+        message: `Person with id ${id} not found`,
+      });
+    }
+
+    return new ApiResponse(true, person);
   }
 
   @Post()
-  async setPerson(@Body() body: IPerson) {
-    storeData.push(body);
+  async setPerson(@Body() body: CreatePerson) {
+    // валідація body використовуючи class-validator
+    const errors = await validate(body);
 
-    return true;
+    if (errors.length > 0) {
+      throw new ApiError(400, {
+        message: 'Validation failed',
+        code: 'PERSON_VALIDATION_ERROR',
+        errors,
+      });
+    }
+
+    const id = storeData.length;
+
+    storeData.push({ ...body, id });
+
+    return new ApiResponse(true, 'Person successfully created');
   }
 }
